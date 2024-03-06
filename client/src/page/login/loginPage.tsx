919769f2f1220cc242bcb63b71cca3d55696b089
Krittamate Cherdpongtagit <kikikrittamate@gmail.com>
2024-03-07 00:43:25 +0700

    add icons and change words

--- client/src/page/login/loginPage.tsx
+++ client/src/page/login/loginPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { z } from "zod";
 
 // Define the login schema using Zod
@@ -7,62 +8,66 @@ const loginSchema = z.object({
   password: z.string().min(8).max(255),
 });
 
-// Define the Login component
 export default function Login() {
-  // Define state variables for form data and form error
   const [formData, setFormData] = useState<LoginForm>({
     username: "",
     password: "",
   });
   const [isFormError, setIsFormError] = useState<boolean>(false);
 
-  // Handle form submission
   const onSubmit = () => {
-    // Validate form data against the login schema
     const validation = loginSchema.safeParse(formData);
 
     if (!validation.success) {
-      // If validation fails, set form error flag
       setIsFormError(true);
     } else {
-      // If validation succeeds, proceed with sign-in
-      // Here you would typically call your authentication API
-      // For demonstration, I'm just logging success
       console.log("Sign-in successful");
     }
   };
 
-  // Handle input change for username field
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, username: e.target.value });
   };
 
-  // Handle input change for password field
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, password: e.target.value });
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12 bg-white gap-8">
+      <div>
+        <a href={"/"}>
+          <img
+              className="absolute top-0 right-0 mr-6 mt-6"
+              src={"/icons/close.svg"}
+              alt={"close"}
+              height={16}
+              width={16}
+          />
+        </a>
+        <div className="min-h-[50%] min-w-[50%]">
+            <img alt="chomromku" src="/logo.svg" className="w-[200px] h-auto" />
+        </div>
+      </div>
       <div className="w-full flex flex-col items-start justify-start gap-2">
-        <label className="font-bold">Username</label>
+        <label className="font-bold">บัญชีผู้ใช้เครือข่ายนนทรี</label>
         <input
           className="w-full mb-1 rounded-full py-1.5 px-3 border border-gray-100 text-sm placeholder:text-sm"
-          placeholder="Enter username"
+          placeholder="เช่น b63xxxxxxxx หรือ regxxx"
           value={formData.username}
           onChange={handleUsernameChange}
         />
-        <label className="font-bold">Password</label>
+        <label className="font-bold">รหัสผ่าน</label>
         <input
           className="w-full rounded-full py-1.5 px-3 border border-gray-100 placeholder:text-sm"
-          placeholder="Enter password"
+          placeholder="รหัสผ่านบัญชีผู้ใช้เครือข่ายนนทรี"
           type="password"
           value={formData.password}
           onChange={handlePasswordChange}
         />
       </div>
       {isFormError ? (
-        <p className="text-red-400">Invalid username or password</p>
+        <p className="text-red-400">บัญชีผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง</p>
       ) : (
         <></>
       )}
@@ -70,14 +75,10 @@ export default function Login() {
         onClick={onSubmit}
         className="rounded-full py-1.5 px-6 bg-[#006664] text-white text-sm"
       >
-        Sign In
+        เข้าสู่ระบบ
       </button>
     </main>
   );
 }
 
-// Define LoginForm type
-type LoginForm = {
-  username: string;
-  password: string;
-};
+type LoginForm = Record<"username" | "password", string>;
