406b00cf1010412fef00333712c58119e24e02b2
Krittamate Cherdpongtagit <kikikrittamate@gmail.com>
2024-04-06 16:34:41 +0700

    fix footer, increase chomromku icon size

--- client/src/pages/components/Navbar.tsx
+++ client/src/pages/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 	return (
 		<nav className="bg-white w-full flex items-center justify-between h-16 px-4 text-[#006664] border-b border-b-[#F2F2F2] relative">
 			<Link to="/">
-				<img alt="chomromku" src={logo} width="0" height="0" sizes="10vw" className="h-8 w-auto" />
+				<img alt="chomromku" src={logo} width="0" height="0" sizes="10vw" className="h-9 w-auto" />
 			</Link>
 			<ul className="flex flex-row items-center gap-x-3">
 				<li>
